83a3d87faa48f5a21ada9aff290430d06baab2a6
Adnan Arnautović <aarnautovi1@etf.unsa.ba>
2018-04-01 12:12:16 +0200

    Dodane forme u XAMLu i png formatu

--- Projekat/MrezaZaProfesionalnoPovezivanje/Mreza/obj/x86/Debug/XamlTypeInfo.g.cs
+++ Projekat/MrezaZaProfesionalnoPovezivanje/Mreza/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -132,15 +132,17 @@ namespace Mreza.Mreza_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[3];
-            _typeNameTable[0] = "Mreza.MainPage";
+            _typeNameTable = new string[4];
+            _typeNameTable[0] = "Mreza.AdminPanel";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
+            _typeNameTable[3] = "Mreza.MainPage";
 
-            _typeTable = new global::System.Type[3];
-            _typeTable[0] = typeof(global::Mreza.MainPage);
+            _typeTable = new global::System.Type[4];
+            _typeTable[0] = typeof(global::Mreza.AdminPanel);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
+            _typeTable[3] = typeof(global::Mreza.MainPage);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -175,7 +177,8 @@ namespace Mreza.Mreza_XamlTypeInfo
             return -1;
         }
 
-        private object Activate_0_MainPage() { return new global::Mreza.MainPage(); }
+        private object Activate_0_AdminPanel() { return new global::Mreza.AdminPanel(); }
+        private object Activate_3_MainPage() { return new global::Mreza.MainPage(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -187,9 +190,9 @@ namespace Mreza.Mreza_XamlTypeInfo
             switch (typeIndex)
             {
 
-            case 0:   //  Mreza.MainPage
+            case 0:   //  Mreza.AdminPanel
                 userType = new global::Mreza.Mreza_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
-                userType.Activator = Activate_0_MainPage;
+                userType.Activator = Activate_0_AdminPanel;
                 userType.SetIsLocalType();
                 xamlType = userType;
                 break;
@@ -200,6 +203,13 @@ namespace Mreza.Mreza_XamlTypeInfo
 
             case 2:   //  Windows.UI.Xaml.Controls.UserControl
                 xamlType = new global::Mreza.Mreza_XamlTypeInfo.XamlSystemBaseType(typeName, type);
+                break;
+
+            case 3:   //  Mreza.MainPage
+                userType = new global::Mreza.Mreza_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_3_MainPage;
+                userType.SetIsLocalType();
+                xamlType = userType;
                 break;
             }
             return xamlType;
